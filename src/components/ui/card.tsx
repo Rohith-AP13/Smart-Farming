18874cda5f729c82584ceee324b74539b79b307e
A.P.Rohith <rainarohith24@gmail.com>
2025-09-07 04:23:19 +0000

    why showing sign up failed

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,3 +1,4 @@
+
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
@@ -33,7 +34,7 @@ const CardTitle = React.forwardRef<
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLHeadingElement>
 >(({ className, ...props }, ref) => (
-  <h3
+  <h2
     ref={ref}
     className={cn("text-xl font-semibold leading-none tracking-tight", className)}
     {...props}
